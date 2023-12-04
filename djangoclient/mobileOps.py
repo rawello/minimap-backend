40be1b0a464e9cda060187c52d62e51c5dbcab77
Michell <49607391+rawello@users.noreply.github.com>
2023-12-04 12:07:19 +0400

    ts

--- djangoclient/mobileOps.py
+++ djangoclient/mobileOps.py
@@ -48,9 +48,9 @@ def generate_images(request, build, destinationFrom, destinationTo):
             with open(f'{folder_path}/{i + 1}-{build}.svg', 'w') as f:
                 f.write(databaseObj.svg[i])
                 convert2png(folder_path, f'{i + 1}-{build}.svg')
-        route = routing(build, floors, rooms_ll, destinationTo, destinationFrom, folder_path)
+
         # вызываем метод алгоритма с рисованием
-        if route:
+        if routing(build, floors, rooms_ll, destinationTo, destinationFrom, folder_path):
             convert2svg(folder_path)
             # получаем список файлов в папке
             files = [file for file in os.listdir(folder_path) if "routed.svg" in file]
@@ -68,21 +68,24 @@ def generate_images(request, build, destinationFrom, destinationTo):
                 output_json["maps"] = floors
                 # отправляем жсон
                 response = JsonResponse(output_json)
-                print("мы в ответе")
+                print(folder_path)
                 shutil.rmtree(f'{folder_path}')
                 return response
+            else:
+                shutil.rmtree(f'{folder_path}')
+                return HttpResponse(400)
         else:
             # в любом другом случае плохо
-            print(route)
+            print(folder_path)
             shutil.rmtree(f'{folder_path}')
             return HttpResponse(400)
     except Exception as e:
         print(e, inspect.stack()[0][3])
         shutil.rmtree(f'{folder_path}')
         return HttpResponse(400)
-    # else:
-    #     print(folder_path)
-    #     #shutil.rmtree(f'{folder_path}')
+    else:
+        print(folder_path)
+        #shutil.rmtree(f'{folder_path}')
 
 
 @csrf_exempt
@@ -98,11 +101,37 @@ def connectWithMobile(request, build):
         room = []
         for i in rooms:
             room.append(i)
-        #     floors.append(rooms[i][0])
-        # output_json["floors"] = max(floors)
-        # output_json["rooms"] = room
-        output_json["maps"] = obj.svg
-        return JsonResponse(output_json)
+
+        while True:
+            folder_path = f'{random.randint(0, 10000000)}'
+            if os.path.isdir(f"{folder_path}"):
+                continue
+            else:
+                os.makedirs(folder_path)
+                break
+        for i in range(len(obj.svg)):
+            with open(f'{folder_path}/{i + 1}-{build}.svg', 'w') as f:
+                f.write(obj.svg[i])
+                convert2png(folder_path, f'{i + 1}-{build}.svg')
+
+        convert2svg(folder_path)
+        files = [file for file in os.listdir(folder_path) if ".svg" in file]
+        floors = []
+        if files:
+            for file in files:
+                with open(f'{folder_path}/{file}', 'r') as file1:
+                    data = file1.read().replace('\n', '').replace('\\', '')
+                floors.append(data)
+
+            output_json["maps"] = floors
+            response = JsonResponse(output_json)
+
+            shutil.rmtree(f'{folder_path}')
+
+            return response
+        else:
+            shutil.rmtree(f'{folder_path}')
+            return HttpResponse(400)
     except Exception as e:
         print(e, inspect.stack()[0][3])
         return HttpResponse(400)
